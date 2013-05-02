72f2d68eaae3abbc6570f01ea012d0433f4fdbb9
Alvin Vogelzang <alvinvogelzang@gmail.com>
2013-05-02 13:41:18 +0200

    Database etc

--- Resources/gebruikersToevoegen.js
+++ Resources/gebruikersToevoegen.js
@@ -47,7 +47,9 @@ btnSubmit.addEventListener('click',function(e) {
 		var dbData = {
 			deelnemer_naam: deelnemer_naam.value
 		};
-		var theData = db.execute('INSERT INTO deelnemers (naam) VALUES ("'+deelnemer_naam.value+'")');
+		// var selectGroepID = db.execute('SELECT * FROM deelnemersgroep WHERE groepID = (SELECT MAX(groepID) FROM deelnemersgroep)');
+		var theData = db.execute('INSERT INTO deelnemers (naam, score) VALUES ("'+deelnemer_naam.value+'", 0)');
+		var theData = db.execute('INSERT INTO deelnemers (groepID) SELECT groepID FROM deelnemersgroep WHERE deelnemersgroep.groepID = (SELECT MAX(groepID) FROM deelnemersgroep)');
 		kaart.open();
 	};
 
