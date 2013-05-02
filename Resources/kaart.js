b05f9810f160f2c5b6b7812fd1d9d5a48b7ad0b1
Alvin Vogelzang <alvinvogelzang@gmail.com>
2013-05-02 11:00:42 +0200

    Annotatie verwijderd

--- Resources/kaart.js
+++ Resources/kaart.js
@@ -1,24 +1,12 @@
 var kaart = Titanium.UI.createWindow();
 
-var mountainView = Titanium.Map.createAnnotation({
-    latitude:37.390749,
-    longitude:-122.081651,
-    title:"Appcelerator Headquarters",
-    subtitle:'Mountain View, CA',
-    pincolor:Titanium.Map.ANNOTATION_RED,
-    animate:true,
-    leftButton: '../images/appcelerator_small.png',
-    myid:1 // Custom property to uniquely identify this annotation.
-});
-
 var mapview = Titanium.Map.createView({
     mapType: Titanium.Map.STANDARD_TYPE,
     region: {latitude:33.74511, longitude:-84.38993, 
             latitudeDelta:0.01, longitudeDelta:0.01},
     animate:true,
     regionFit:true,
-    userLocation:true,
-    annotations:[mountainView]
+    userLocation:true
 });
 
 kaart.add(mapview);
